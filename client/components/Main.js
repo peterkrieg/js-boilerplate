f36a39b8c2ddb635a5e0c7f725a36a95bb586c4c
Peter Krieg <pkrieg2@gmail.com>
2016-11-29 18:36:34 -0500

    fixed versioning, added eslintrc

--- client/components/Main.js
+++ client/components/Main.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const Main = React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>
-          <Link to="/">Reduxstagram</Link>
-        </h1>
-        <h2>Hello world!</h2>
-        <button>Click to increment</button>
-        {React.cloneElement({...this.props}.children, {...this.props})}
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<h1>
+					<Link to="/">Reduxstagram</Link>
+				</h1>
+				<h2>Hello world!</h2>
+				<button>Click to increment</button>
+				{React.cloneElement({...this.props}.children, {...this.props})}
+			</div>
+		)
+	}
 });
 
 export default Main;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -16,14 +16,18 @@ import { Provider } from 'react-redux';
 import store, { history } from './store';
 
 const router = (
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRoute component={PhotoGrid}></IndexRoute>
-        <Route path="/view/:postId" component={Single}></Route>
-      </Route>
-    </Router>
-  </Provider>
-)
+	<Provider store={store}>
+		<Router history={history}>
+			<Route path="/" component={App}>
+				<IndexRoute component={PhotoGrid} />
+				<Route path="/view/:postId" component={Single} />
+			</Route>
+		</Router>
+	</Provider>
+);
 
 render(router, document.getElementById('root'));
+
+
+
+
